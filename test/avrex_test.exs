b47d3402899a4f038a45fb7b88b7ed5108a89ada
Tame, Dan <Dan.Tame@sky.uk>
2016-02-10 10:32:55 +0000

    Add tests for positive and negative numbers

--- test/avrex_test.exs
+++ test/avrex_test.exs
@@ -2,20 +2,36 @@ defmodule AvrexTest do
   use ExUnit.Case
   doctest Avrex
 
-  test "should encode an integer correctly" do
-    assert Avrex.encode(:int, 1) == <<2>>
+  test "should encode a positive integer correctly" do
+    assert Avrex.encode(:int, 101) == <<202, 1>>
   end
 
-  test "should encode a long correctly" do
+  test "should encode a negative integer correctly" do
+    assert Avrex.encode(:int, -101) == <<201, 1>>
+  end
+
+  test "should encode a positive long correctly" do
     assert Avrex.encode(:long, 123456789012345678901234567890) == <<201, 148, 176, 141, 248, 187, 240, 243, 134, 1>>
   end
 
-  test "should encode a float correctly" do
+  test "should encode a negative long correctly" do
+    assert Avrex.encode(:long, -123456789012345678901234567890) == <<206, 148, 176, 141, 248, 187, 240, 243, 134, 1>>
+  end
+
+  test "should encode a positive float correctly" do
     assert Avrex.encode(:float, 1.23) == <<164, 112, 157, 63>>
   end
 
-  test "should encode a double correctly" do
+  test "should encode a negative float correctly" do
+    assert Avrex.encode(:float, -1.23) == <<164, 112, 157, 191>>
+  end
+
+  test "should encode a positive double correctly" do
     assert Avrex.encode(:double, 1.23456789012345678901234567890) == <<251, 89, 140, 66, 202, 192, 243, 63>>
+  end
+
+  test "should encode a negative double correctly" do
+    assert Avrex.encode(:double, -1.23456789012345678901234567890) == <<251, 89, 140, 66, 202, 192, 243, 191>>
   end
 
   test "should encode a string correctly" do
