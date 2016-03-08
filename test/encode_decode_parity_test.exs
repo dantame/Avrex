6145af82262b63c42eb02879768ce842e98b2d4f
Tame, Dan <Dan.Tame@sky.uk>
2016-03-08 17:45:56 +0000

    Change type to 2nd arg to enable better piping

--- test/encode_decode_parity_test.exs
+++ test/encode_decode_parity_test.exs
@@ -6,105 +6,105 @@ defmodule EncodeDecodeParityTest do
 
   property "strings" do
     forall val <- utf8 do
-      {decoded, _} = Avrex.decode(:string, Avrex.encode(:string, val))
+      {decoded, _} = Avrex.decode(Avrex.encode(val, :string), :string)
       assert val == decoded
     end
   end
 
   property "integers" do
     forall val <- int do
-      {decoded, _} = Avrex.decode(:int, Avrex.encode(:int, val))
+      {decoded, _} = Avrex.decode(Avrex.encode(val, :int), :int)
       assert val == decoded
     end
   end
 
   property "longs" do
     forall val <- largeint do
-      {decoded, _} = Avrex.decode(:long, Avrex.encode(:long, val))
+      {decoded, _} = Avrex.decode(Avrex.encode(val, :long), :long)
       assert val == decoded
     end
   end
 
   property "boolean" do
     forall val <- bool do
-      {decoded, _} = Avrex.decode(:boolean, Avrex.encode(:boolean, val))
+      {decoded, _} = Avrex.decode(Avrex.encode(val, :boolean), :boolean)
       assert val == decoded
     end
   end
 
   property "binary" do
     forall val <- binary do
-      {decoded, _} = Avrex.decode(:bytes, Avrex.encode(:bytes, val))
+      {decoded, _} = Avrex.decode(Avrex.encode(val, :bytes), :bytes)
       assert val == decoded
     end
   end
 
   property "large binary" do
     forall val <- largebinary do
-      {decoded, _} = Avrex.decode(:bytes, Avrex.encode(:bytes, val))
+      {decoded, _} = Avrex.decode(Avrex.encode(val, :bytes), :bytes)
       assert val == decoded
     end
   end
 
   property "float" do
     forall val <- real do
-      {decoded, _} = Avrex.decode(:float, Avrex.encode(:float, val))
+      {decoded, _} = Avrex.decode(Avrex.encode(val, :float), :float)
       assert Float.round(val, 3) == Float.round(decoded, 3)
     end
   end
 
   property "double" do
     forall val <- real do
-      {decoded, _} = Avrex.decode(:double, Avrex.encode(:double, val))
+      {decoded, _} = Avrex.decode(Avrex.encode(val, :double), :double)
       assert val == decoded
     end
   end
 
   property "string array" do
     forall val <- list(utf8) do
-      {decoded, _} = Avrex.decode({:array, :string}, Avrex.encode({:array, :string}, val))
+      {decoded, _} = Avrex.decode(Avrex.encode(val, {:array, :string}), {:array, :string})
       assert val == decoded
     end
   end
 
   property "integer array" do
     forall val <- list(int) do
-      {decoded, _} = Avrex.decode({:array, :int}, Avrex.encode({:array, :int}, val))
+      {decoded, _} = Avrex.decode(Avrex.encode(val, {:array, :int}), {:array, :int})
       assert val == decoded
     end
   end
 
   property "long array" do
     forall val <- list(largeint) do
-      {decoded, _} = Avrex.decode({:array, :long}, Avrex.encode({:array, :long}, val))
+      {decoded, _} = Avrex.decode(Avrex.encode(val, {:array, :long}), {:array, :long})
       assert val == decoded
     end
   end
 
   property "boolean array" do
     forall val <- list(bool) do
-      {decoded, _} = Avrex.decode({:array, :boolean}, Avrex.encode({:array, :boolean}, val))
+      {decoded, _} = Avrex.decode(Avrex.encode(val, {:array, :boolean}), {:array, :boolean})
       assert val == decoded
     end
   end
 
   property "binary array" do
     forall val <- list(binary) do
-      {decoded, _} = Avrex.decode({:array, :bytes}, Avrex.encode({:array, :bytes}, val))
+      {decoded, _} = Avrex.decode(Avrex.encode(val, {:array, :bytes}), {:array, :bytes})
       assert val == decoded
     end
   end
 
   property "float array" do
     forall val <- list(real) do
-      {decoded, _} = Avrex.decode({:array, :float}, Avrex.encode({:array, :float}, val))
+      {decoded, _} = Avrex.decode(Avrex.encode(val, {:array, :float}), {:array, :float})
       assert val == decoded
     end
   end
 
   property "double array" do
     forall val <- list(real) do
-      {decoded, _} = Avrex.decode({:array, :double}, Avrex.encode({:array, :double}, val))
+      {decoded, _} = Avrex.decode(Avrex.encode(val, {:array, :double}), {:array, :double})
       assert val == decoded
     end
   end
--- test/encode_test.exs
+++ test/encode_test.exs
@@ -2,55 +2,55 @@ defmodule EncodeTest do
   use ExUnit.Case
 
   test "should encode a positive integer correctly" do
-    assert Avrex.encode(:int, 101) == <<202, 1>>
+    assert Avrex.encode(101, :int) == <<202, 1>>
   end
 
   test "should encode a negative integer correctly" do
-    assert Avrex.encode(:int, -101) == <<201, 1>>
+    assert Avrex.encode(-101, :int) == <<201, 1>>
   end
 
   test "should encode a positive long correctly" do
-    assert Avrex.encode(:long, 123456789012345678901234567890) == <<201, 148, 176, 141, 248, 187, 240, 243, 134, 1>>
+    assert Avrex.encode(123456789012345678901234567890, :long) == <<201, 148, 176, 141, 248, 187, 240, 243, 134, 1>>
   end
 
   test "should encode a negative long correctly" do
-    assert Avrex.encode(:long, -123456789012345678901234567890) == <<206, 148, 176, 141, 248, 187, 240, 243, 134, 1>>
+    assert Avrex.encode(-123456789012345678901234567890, :long) == <<206, 148, 176, 141, 248, 187, 240, 243, 134, 1>>
   end
 
   test "should encode a positive float correctly" do
-    assert Avrex.encode(:float, 1.23) == <<164, 112, 157, 63>>
+    assert Avrex.encode(1.23, :float) == <<164, 112, 157, 63>>
   end
 
   test "should encode a negative float correctly" do
-    assert Avrex.encode(:float, -1.23) == <<164, 112, 157, 191>>
+    assert Avrex.encode(-1.23, :float) == <<164, 112, 157, 191>>
   end
 
   test "should encode a positive double correctly" do
-    assert Avrex.encode(:double, 1.23456789012345678901234567890) == <<251, 89, 140, 66, 202, 192, 243, 63>>
+    assert Avrex.encode(1.23456789012345678901234567890, :double) == <<251, 89, 140, 66, 202, 192, 243, 63>>
   end
 
   test "should encode a negative double correctly" do
-    assert Avrex.encode(:double, -1.23456789012345678901234567890) == <<251, 89, 140, 66, 202, 192, 243, 191>>
+    assert Avrex.encode(-1.23456789012345678901234567890, :double) == <<251, 89, 140, 66, 202, 192, 243, 191>>
   end
 
   test "should encode a string correctly" do
-    assert Avrex.encode(:string, "test") == <<8, 116, 101, 115, 116>>
+    assert Avrex.encode("test", :string) == <<8, 116, 101, 115, 116>>
   end
 
   test "should encode bytes correctly" do
-    assert Avrex.encode(:bytes, <<1, 2, 3, 4>>) == <<8, 1, 2, 3, 4>>
+    assert Avrex.encode(<<1, 2, 3, 4>>, :bytes) == <<8, 1, 2, 3, 4>>
   end
 
   test "should encode a truthy boolean correctly" do
-    assert Avrex.encode(:boolean, true) == <<1>>
+    assert Avrex.encode(true, :boolean) == <<1>>
   end
 
   test "should encode a falsy boolean correctly" do
-    assert Avrex.encode(:boolean, false) == <<0>>
+    assert Avrex.encode(false, :boolean) == <<0>>
   end
 
   test "should return an empty binary for unknown types" do
-    assert Avrex.encode(:unknown, 1234) == <<>>
-    assert Avrex.encode(:zoidberg, "werglebergle") == <<>>
+    assert Avrex.encode(1234, :unknown) == <<>>
+    assert Avrex.encode("werglebergle", :zoidberg) == <<>>
   end
 end
